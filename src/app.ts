5139eab9a2c5b026afff31c4e2f2a878ae87074a
Kemal Kazanc <keemalkazanc@gmail.com>
2024-03-14 17:24:34 +0100

    update début de l'exercice2

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+/*
+
 const compteur = document.querySelector("#compteur");
 let i = 0;
 
@@ -8,3 +10,21 @@ const increment = (e) => {
 };
 
 compteur.addEventListener("click", increment);
+*/
+// Exercice 2 : Afficher
+
+const a: string = "Hello World";
+const n: number = 3;
+const b: boolean = true;
+const d: null = null;
+const arr: string[] = ["aze", "aze", "3"];
+const user: { firstName: string; lastName: string } = {
+  firstName: "John",
+  lastName: "Doe",
+};
+const date: Date = new Date();
+const cb: Function = (e: MouseEvent) => {};
+
+function printId(id: number): void {
+  console.log(id.toString());
+}
